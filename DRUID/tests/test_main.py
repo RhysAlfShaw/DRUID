34d0dbd2ba27a45a3b992566e793f1bd1476fd09
Rhys Shaw <rhysc3p0@gmail.com>
2024-02-07 11:33:01 +0000

    fixed failing tests

--- DRUID/tests/test_main.py
+++ DRUID/tests/test_main.py
@@ -1,10 +1,16 @@
 from DRUID.main import sf
 import pytest
+import numpy as np
 
 def test_sf():
     '''
     Test initalisation of the main Class
     '''
-    assert sf(image=None, image_path=None,mode="Radio",pb_path=None)
+    arr2d = np.random.rand(100,100)
+    assert sf(image=arr2d, image_path=None,
+              mode="Radio",pb_path=None,cutup=False,
+              cutup_size=None,cutup_buff=None,output=False,
+              area_limit=5,smooth_sigma=1,nproc=1,GPU=True,
+              header=None,Xoff=None,Yoff=None) != None
     
     
