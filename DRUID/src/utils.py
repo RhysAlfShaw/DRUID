3a840395b48090af8a9c4249b4ede1c02a00cb8e
Rhys Shaw <rhysc3p0@gmail.com>
2025-07-16 09:55:23 +0100

    fixed loading images when dim>2

--- DRUID/src/utils.py
+++ DRUID/src/utils.py
@@ -20,6 +20,12 @@ def get_image_from_path(image_path):
     with fits.open(image_path) as hdul:
         image = hdul[0].data
 
+    # warn if the image is not 2D
+    # reduce the image to 2D if it is not
+    if image.ndim == 3:
+        image = image[0, :, :]
+    elif image.ndim == 4:
+        image = image[0, 0, :, :]
     return image
 
 
